1561be63c5f33d449c6f3bdfbe81fd0f570064c6
dharminder <144053986+DharminderPal@users.noreply.github.com>
2024-12-23 14:42:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+function numberlo(){
+
+return new Promise((resolve,reject)=>{
+// document.getElementById("output")
+setTimeout(()=>{
+resolve([1,2,3,4])
+},3000)
+});
+}
+
+
+
+numberlo()
+.then((koi_value_pass_kro)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+const evenNumber= koi_value_pass_kro.filter((pass_the_value)=>pass_the_value % 2 === 0);
+document.getElementById("output").textContent =evenNumber.join(", ");
+resolve(evenNumber);
+},1000)
+    })
+})
+
+.then((multiply)=>{
+return  new Promise((resolve,reject)=>{
+setTimeout(()=>{
+const multi = evenNumber.map((pass_any_value)=pass_any_value*2);
+document.getElementById("output").textContent=multi.join(", ");
+resolve(multi);
+},2000)
+});
+})
+.catch((error)=>{
+console.log(error);
+})
